3e0c4e1296e1a5cadc83e46631d69acafbc416cf
nariks <happycow@gmail.com>
2016-03-02 22:32:27 +1100

    Added add_to_products, find_by_title and in_stock methods

--- lib/product.rb
+++ lib/product.rb
@@ -4,15 +4,32 @@ class Product
   @@products = []
 
   def initialize(options={})
-
-    @@products.push(self)
-
+    @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+    add_to_products
   end
 
   def self.all
-
     @@products
+  end
 
+  def add_to_products
+    @@products.each do |product|
+      raise DuplicateProductError, "#{@title} already exists \n"  if product.title == @title
+    end
+
+    @@products << self
+  end
+
+  def self.find_by_title(title)
+    @@products.each do |product|
+        return product if product.title == title
+    end
+  end
+
+  def in_stock?
+    @stock > 0 ? true : false
   end
 
 
